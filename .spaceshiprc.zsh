836b0e6fea8f60ba752a93d353d3c57e6c137d20
Nick Seman <me@vphoebe.dev>
2025-05-13 16:21:33 -0700

    spaceship config

---
+++ .spaceshiprc.zsh
@@ -0,0 +1,14 @@
+# use % for prompt
+SPACESHIP_CHAR_SYMBOL="%% "
+
+# hide stashed icon for git
+SPACESHIP_GIT_STATUS_STASHED=""
+
+# right-side prompt
+SPACESHIP_RPROMPT_ORDER=(user host)
+SPACESHIP_HOST_SHOW="always"
+SPACESHIP_USER_SHOW="always"
+SPACESHIP_HOST_PREFIX="@"
+SPACESHIP_USER_SUFFIX=""
+spaceship remove user
+spaceship remove host
